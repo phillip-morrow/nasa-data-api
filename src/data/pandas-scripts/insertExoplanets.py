118f9070af7f6deadff406afd8e84b7b00e00ebe
pmorrowtron <phillip.l.morrow@gmail.com>
2021-07-28 22:05:12 -0500

    added sql server and started figuring out roundhouse migrations service

--- src/data/pandas-scripts/insertExoplanets.py
+++ src/data/pandas-scripts/insertExoplanets.py
@@ -3,3 +3,8 @@ import pandas as pd
 
 def insert_exoplanets():
     return pd.DataFrame(data='../seed-data/exoplanets-2021.csv')
+
+
+df = insert_exoplanets()
+
+df.head(1)
---
+++ src/utils/database_conn.py
@@ -0,0 +1,10 @@
+from sqlalchemy import create_engine
+
+
+def make_connection():
+    server = "localhost"
+    database = "master"
+    user = "sa"
+    password = "secrets123!"
+
+    return create_engine(f"mssql://{user}:{password}@{server}/database")
